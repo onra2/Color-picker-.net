6fcebbbfbc02f2712cec6b5d360e13302fa1eb4d
onra2 <arno.pion007@gmail.com>
2021-09-18 16:21:59 +0200

    first commit

---
+++ Onra2 Color Picker/Main.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Onra2_Color_Picker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.labelRGB = new MaterialSkin.Controls.MaterialLabel();
+            this.MouseMoveTimer = new System.Windows.Forms.Timer(this.components);
+            this.NewButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelRGB
+            // 
+            this.labelRGB.AutoSize = true;
+            this.labelRGB.Depth = 0;
+            this.labelRGB.Font = new System.Drawing.Font("Roboto", 11F);
+            this.labelRGB.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelRGB.Location = new System.Drawing.Point(13, 67);
+            this.labelRGB.MouseState = MaterialSkin.MouseState.HOVER;
+            this.labelRGB.Name = "labelRGB";
+            this.labelRGB.Size = new System.Drawing.Size(97, 19);
+            this.labelRGB.TabIndex = 0;
+            this.labelRGB.Text = "R: 0 G: 0 B: 0 ";
+            // 
+            // MouseMoveTimer
+            // 
+            this.MouseMoveTimer.Interval = 10;
+            this.MouseMoveTimer.Tick += new System.EventHandler(this.MouseMoveTimer_Tick);
+            // 
+            // NewButton
+            // 
+            this.NewButton.AutoSize = true;
+            this.NewButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.NewButton.Depth = 0;
+            this.NewButton.Icon = null;
+            this.NewButton.Location = new System.Drawing.Point(13, 92);
+            this.NewButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.NewButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Primary = false;
+            this.NewButton.Size = new System.Drawing.Size(51, 36);
+            this.NewButton.TabIndex = 1;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 143);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.labelRGB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Picker";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel labelRGB;
+        private System.Windows.Forms.Timer MouseMoveTimer;
+        private MaterialSkin.Controls.MaterialFlatButton NewButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Onra2 Color Picker/Main.cs
@@ -0,0 +1,158 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Onra2_Color_Picker
+{
+    public partial class Main : MaterialForm
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto),]
+        public static extern int SystemParametersInfo(uint uiAction, uint uiParam, uint pvParam, uint fWinIni);
+
+        Graphics g;
+        Bitmap bmp;
+
+        Form form2;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue700, Primary.Blue500, Primary.Blue700, Accent.Cyan700, TextShade.WHITE);
+        }
+
+        private void MouseMoveTimer_Tick(object sender, EventArgs e)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+            BackColor = c;
+
+            labelRGB.Text = "R: " + c.R + " G: " + c.G + " B: " + c.B;
+            Clipboard.SetText(c.R + ", " + c.G + ", " + c.B);
+
+            var endWidth = pictureBox1.Width;
+            var endHeight = pictureBox1.Height;
+
+            const int scaleFactor = 5;
+
+            var startWidth = endWidth / scaleFactor;
+            var startHeight = endHeight / scaleFactor;
+
+            bmp = new Bitmap(startWidth, startHeight);
+
+            g = CreateGraphics();
+            g = Graphics.FromImage(bmp);
+
+            var xPos = Math.Max(0, MousePosition.X - (startWidth / 2));
+            var yPos = Math.Max(0, MousePosition.Y - (startHeight / 2));
+
+            g.CopyFromScreen(xPos, yPos, 0, 0, new Size(endWidth, endWidth));
+            pictureBox1.Image = bmp;
+
+            //move window
+            Rectangle activeScreenDimensions = Screen.FromControl(this).Bounds;
+            Left = (MousePosition.X < activeScreenDimensions.Width - this.Width) ? MousePosition.X + 20 : MousePosition.X - this.Width - 20;
+            Top = (MousePosition.Y < activeScreenDimensions.Height - this.Height) ? MousePosition.Y + 20 : MousePosition.Y - this.Height - 20;
+            TopMost = true;
+        }
+
+        private readonly Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+            Selection();
+            MouseMoveTimer.Start();
+        }
+
+        private void Selection()
+        {
+            form2 = new Form
+            {
+                Opacity = 0.01,
+                Cursor = Cursors.Cross,
+                ControlBox = false,
+                MaximizeBox = false,
+                MinimizeBox = false,
+                FormBorderStyle = FormBorderStyle.None,
+                WindowState = FormWindowState.Maximized
+            };
+            form2.MouseUp += form2_MouseUp;
+
+            form2.Show();
+        }
+
+        private void form2_MouseUp(object sender, MouseEventArgs e)
+        {
+            form2.Hide();
+            form2.Close();
+            MouseMoveTimer.Stop();
+            Show();
+            uint speed = 10;
+            int res = SystemParametersInfo(113, 0, speed, 0);
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((ModifierKeys & Keys.Control) == Keys.Control)
+            {
+                uint speed = 3;
+                int res = SystemParametersInfo(113, 0, speed, 0);
+            }
+            if ((ModifierKeys & Keys.Shift) == Keys.Shift)
+            {
+                uint speed = 10;
+                int res = SystemParametersInfo(113, 0, speed, 0);
+            }
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            uint speed = 10;
+            int res = SystemParametersInfo(113, 0, speed, 0);
+        }
+    }
+}
